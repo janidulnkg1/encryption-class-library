3314c942a6c8254a4d1ecad9d9d70dae7246d18e
janidulnkg1 <janidulnkg1@gmail.com>
2023-09-27 16:01:28 +0530

    add keyprovider interface and class

---
+++ encryption-cl/encryption-cl/Key/KeyProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encryption_cl
+{
+    public class KeyProvider
+    {
+        private byte[] key;
+
+        public byte[] GetKey()
+        {
+            return key;
+        }
+
+        public void SetKey(byte[] key)
+        {
+            this.key = key;
+        }
+    }
+}
